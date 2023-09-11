dec994992a6027dcd23a0278e5f6831582fa5907
Jaosn Tsang <98151972+Jasontth@users.noreply.github.com>
2023-09-11 11:10:59 +0800

    Update counter1.js

--- blogs/counter1.js
+++ blogs/counter1.js
@@ -16,26 +16,31 @@ firebase.initializeApp(firebaseConfig);
 // Get a reference to the blog post in the Firebase Realtime Database
 const postRef = firebase.database().ref("blog/blog1");
 
-// Get the user's IP address
-const getIPAddress = async () => {
-    const response = await fetch("https://api.ipify.org?format=json");
-    const data = await response.json();
-    return data.ip;
-  };
+// Get the number of unique IP addresses in the database
+function getUniqueIPCount() {
+    return postRef
+      .child("views")
+      .orderByChild("ipAddress")
+      .once("value")
+      .then((snapshot) => {
+        const uniqueIPs = new Set();
+        snapshot.forEach((childSnapshot) => {
+          uniqueIPs.add(childSnapshot.val().ipAddress);
+        });
+        return uniqueIPs.size;
+      });
+  }
   
-  // Increment the view count in the database
-  const incrementViewCount = async () => {
-    const ipAddress = await getIPAddress();
-    postRef.transaction((post) => {
-      if (post) {
-        if (!post.views) {
-          post.views = {};
-        }
-        if (!post.views[ipAddress]) {
-          post.views[ipAddress] = true;
-          post.viewCount = Object.keys(post.views).length;
-        }
-      }
-      return post;
+  // Call the function to get the unique IP count and update the display
+  getUniqueIPCount().then((uniqueIPCount) => {
+    const uniqueIPCountElement = document.getElementById("uniqueIPCount");
+    uniqueIPCountElement.innerText = uniqueIPCount;
+  });
+  
+  // Listen for changes to the view count in the database and update the display
+  postRef.child("views").on("value", (snapshot) => {
+    getUniqueIPCount().then((uniqueIPCount) => {
+      const uniqueIPCountElement = document.getElementById("uniqueIPCount");
+      uniqueIPCountElement.innerText = uniqueIPCount;
     });
-  };
+  });
