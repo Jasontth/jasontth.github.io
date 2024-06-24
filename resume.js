89b5eef3087d0a2d6af975e6b84825237b6a9058
Jaosn Tsang <98151972+Jasontth@users.noreply.github.com>
2024-06-24 23:07:59 +0200

    new update

---
+++ resume.js
@@ -0,0 +1,126 @@
+const resumeDetails = {
+    name: "Jason Tsang",
+    age: 21,
+    location: "Hong Kong",
+    contact: {
+        email: "jasontsangtszhin@gmail.com",
+        linkedin: "https://www.linkedin.com/in/jason-tsangtszhin/",
+        github: "https://github.com/Jasontth",
+        x: "https://x.com/jasontth_x",
+    },
+    summary: "A passionate individual with a strong interest in the intersection of blockchain, AI, and IoT. Enthusiastic about leveraging these technologies to drive innovation and solve real-world challenges.",
+    education: [
+        {
+            institution: "The Chinese University of Hong Kong, Hong Kong (on-going)",
+            period: "Sept 2021 – July 2025 (expected)",
+            degree: "BSSc in Data Science and Policy Studies",
+            gpa: ["Major GPA: 3.73/4.0", "cumulative GPA: 3.67/4.0"],
+            honors: ["Master's List (2022, 2023)", "Dean's List (2023)"]
+        },
+        {
+            institution: "Technical University of Munich, Germany",
+            period: "Apr 2024 – Aug 2024",
+            program: "Semester Exchange in Politics & Technology",
+            awards: ["Reaching Out Award (2024)", "Scholarship for Semester Exchange (2024)"]
+        }
+    ],
+    courses: [
+        {
+            technical: ["Machine Learning", "Deep Learning", "Natural Language Processing", "Blockchain Technology", "Game Theory"],
+            social_science: ["Policy Science", "Sustainability", "Collab Govern", "Psychology", "Research Methods"]
+        }
+    ],
+    workExperience: [
+        {
+            company: "Climind",
+            period: "Sept 2022 – Sept 2023",
+            role: "Research Assistant",
+            responsibilities: [
+                "Conducted web scraping of climate change reports",
+                "Worked on NLP projects focused on building a Retrieval Augmented Generation (RAG) system"
+            ]
+        },
+        {
+            company: "The Chinese University of Hong Kong, Hong Kong",
+            period: "Sept 2022 – Sept 2023",
+            role: "Research Assistant (Part-Time)",
+            responsibilities: [
+                "Analyzed the emigration trend in Hong Kong using both quantitative and qualitative research methods",
+                "Performed web scraping and data mining on social media platforms using Python"
+            ]
+        },
+        {
+            company: "Laboratory for AI-Powered Financial Technologies Limited, Hong Kong",
+            period: "June 2023 – Aug 2023",
+            role: "Data Science Summer Intern",
+            responsibilities: [
+                "Assisted in data cleaning and pre-processing of financial data",
+                "Participated in data scraping projects",
+                "Developed systems for on-chain data analysis",
+                "Deployed machine learning models"
+            ]
+        },
+        {
+            company: "GreenSafety Technology Limited, Hong Kong",
+            period: "Nov 2022 – Feb 2023",
+            role: "STEM Intern",
+            responsibilities: [
+                "Analyzed driving safety and energy consumption patterns",
+                "Led data validation efforts",
+                "Contributed to the development of transportation safety solutions"
+            ]
+        },
+        {
+            company: "Centrum Stosunków Międzynarodowych, Warsaw, Poland",
+            period: "Aug 2022 – Sept 2022",
+            role: "Research Intern",
+            responsibilities: [
+                "Prepared reports on international relations research",
+                "Conducted research on AI and 5G technology"
+            ]
+        },
+        {
+            company: "NEOMA, Hong Kong",
+            period: "June 2022 – Aug 2022",
+            role: "Summer Intern (AI IoT app)",
+            responsibilities: [
+                "Configured and tested data touchpoints and sensors for major projects",
+                "Collaborated on integrating data touchpoints with AI and IoT systems",
+                "Conducted quality assurance"
+            ]
+        }
+    ],
+    research: [
+        {
+            title: "Fine-Tuning Pre-Trained Transformers for Climate Claim Verification",
+            link: "https://jasontth.github.io/researches/climate-claim-verification.html",
+            abstract: "Misinformation and disinformation on the internet present a significant challenge in the context of climate change debate. The dissemination of false or misleading information can hinder public understanding and impede efforts to combat the growing issue of climate change. While social media platforms have implemented automatic fact-checking algorithms, existing models lack domain-specific training to effectively verify climate change-related information. As a remedy, a new fact-checking dataset is proposed that combines data from CLIMATE-FEVER with web-scraped information, resulting in a comprehensive dataset comprising 8,115 annotated claim-evidence pairs. The improved dataset is used to fine-tune a variety of pre-trained transformers for climate claim verification tasks. The best model, RoBERTa, achieved an accuracy of 0.7288 and F1-score of 0.7229, improving upon previously reported state-of-the-art (SoTA) F1-score of 0.7182.",
+            video: "https://www.youtube.com/watch?v=sERNbgyJIZk"
+        },
+        {
+            title: "Machine Learning Approaches for Bitcoin Address Classification: A Comprehensive Review and Dataset",
+            link: "http://dx.doi.org/10.13140/RG.2.2.35652.03200",
+            abstract: "The recent crises and bubble bursts in the crypto market, such as the Terra incident and the collapse of FTX, have raised concerns among government entities and investors. To increase transparency and monitor on-chain activities, reliable methods are needed to identify illegal transactions. Machine learning techniques can serve as valuable tools for classifying addresses and detecting illicit on-chain activities. This paper focuses on Bitcoin, the most popular cryptocurrency, and aims to provide a comprehensive review of the feasibility of using machine learning classification methods for labeling Bitcoin addresses. Additionally, we present a publicly available dataset for address labeling by scraping data from Blockchain.com. The dataset includes 15,355 labeled Bitcoin addresses, as well as labels for Tokens, ETH, and BCH addresses. These resources are made available on GitHub (https://github.com/Jasontth/crypto_label) to facilitate research and enable the training of classification models in this domain."
+        }
+    ],
+    projectsCompetitions: [
+        "Application of Foundational Language Models to Climate Change",
+        "Open Data Hackathon 2022",
+        "College Student Public Administration Data Analysis Contest 2021",
+        "National Youth Future Engineer Exhibition and Competition 2019 - 3rd Runner-up",
+        "The First Future Engineer Grand Challenge 2019 - 1st Runner-up"
+    ],
+    interest: ["Blockchain", "AI", "IoT", "Data Science", "Machine Learning", "Deep Learning", "NLP", "International Relations", "Public Policy", "Sustainable Development", "Climate Change"],
+    hobbies: ["Reading about politics, science", "Coding in Python", "Traveling", "Listening to podcasts from Lex Fridman", "Watch Youtube", "Hit the gym", "occasionally play CS2"],
+    technicalSkills: {
+        technical: ["Advanced in R, SQL, Linux, HTML/CSS", "Proficient in Python"],
+        languages: ["Fluent in English, Cantonese, Chinese"],
+        onlineCourses: [
+            "Mathematics for Machine Learning and Data Science Specialization (Coursera)",
+            "Game Theory (Stanford)",
+            "IBM Machine Learning Specialization (IBM)",
+            "Machine Learning A-Z (Udemy)",
+            "Blockchain Basics (University at Buffalo)"
+        ]
+    }
+};
